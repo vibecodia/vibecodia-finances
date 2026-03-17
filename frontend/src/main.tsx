12db09fb825b34684c23085541b4d2e5a4080d68
Rodolfo Neto <rodolfotiago@gmail.com>
2026-03-17 18:34:09 -0300

    Feature/show saldo (#104)
    * wip
    
    * wip

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import '@fontsource/kalam/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <ThemeProvider>
         <VerificationProvider>
           <App />
