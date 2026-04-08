df55fc9b8f5b524011b820d28ed29f04a5df441d
dodopontocom <rodolfotiago@gmail.com>
2026-04-08 19:59:15 -0300

    wip: toggle vales

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { format, getDate, getDaysInMonth, isBefore, startOfMonth, endOfMonth } from 'date-fns';
-import { Wallet, Target, AlertTriangle, CreditCard, Eye, EyeOff } from 'lucide-react';
+import { Target, AlertTriangle, CreditCard, Eye, EyeOff } from 'lucide-react';
 import React, { useEffect, useState } from 'react';
 import Confetti from 'react-confetti';
 import { useNavigate } from 'react-router-dom';
